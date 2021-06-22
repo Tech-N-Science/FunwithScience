9460c8b6c4c356c134016fdc1e6c56208d700c36
Amit kumar mishra <akm43300@gmail.com>
2021-06-22 11:32:55 +0530

    commit:

--- funwithpysics/src/App.js
+++ funwithpysics/src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-         Fun With Physics
-        </p>
-        
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
