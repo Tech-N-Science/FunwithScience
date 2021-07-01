72b6f6f56c1c3a1a4971846928ffade0a26ddf71
Amit kumar mishra <akm43300@gmail.com>
2021-07-01 17:40:25 +0530

    update

--- funwithpysics/src/Components/Navbar/Navbar.js
+++ funwithpysics/src/Components/Navbar/Navbar.js
@@ -4,12 +4,12 @@ const Navbar = () => {
     return (
         <>
          <nav className="navbar navbar-expand-lg navbar-light bg-light pt-3">
-            <a className="navbar-brand ml-5" href="#"> <i class="fas fa-book"></i><i class="fas fa-book"></i><i class="fas fa-book"></i> Fun With Physics</a>
+            <p style={{padding: "0px", fontSize: "30px"}} className="navbar-brand ml-5" href="#"> <i class="fas fa-book"></i><i class="fas fa-book"></i><i class="fas fa-book"></i> Fun With Physics</p>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto mr-5">
+                <ul style={{fontSize: "20px"}}className="navbar-nav ml-auto mr-5">
                 <NavLink to='/' className="nav-item ">
                     <a className="nav-link" >Home</a>
                 </NavLink>
