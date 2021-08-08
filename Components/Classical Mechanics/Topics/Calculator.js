ce6bd2a9663faaacaafcc3a63f5286cff5eb15bd
Hitesh Sukhwani <55627571+Hiteshsukhwani29@users.noreply.github.com>
2021-08-09 00:26:06 +0530

    Add Work Power Energy Page

---
+++ Components/Classical Mechanics/Topics/Calculator.js
@@ -0,0 +1,383 @@
+import React, { useState } from 'react'
+import './Calculator.css'
+import Topics from '../topics_data'
+import { Form, Card, Button } from 'react-bootstrap'
+import '../classicalMechanics.css'
+import WPE_list from "../wpe_data";
+
+function Calculator({ match }) {
+    const page = Topics.filter(data => (data.topic) === (match.params.topic))
+    const details = page[0]
+    console.log(page)
+    console.log(details.formula)
+
+    // Momentum Calculator
+    function CalculatorMomentum() {
+        const [result, setResult] = useState(null)
+        const [mass, setMass] = useState(null)
+        const [vel, setVel] = useState(null)
+
+        const handleClick = () => {
+            let res = mass * vel;
+            setResult(res)
+        }
+        return <React.Fragment>
+            <Form>
+
+                <Form.Group className="mb-3" controlId="mass">
+                    <Form.Label>Mass (M)</Form.Label>
+                    <Form.Control onChange={(e) => setMass(e.target.value)} type="number" placeholder="Enter Mass in SI unit" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="velocity">
+                    <Form.Label>Velocity (V)</Form.Label>
+                    <Form.Control onChange={(e) => setVel(e.target.value)} type="number" placeholder="Enter Velocity in SI unit" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="momentum">
+                    <Form.Label>Momentum (P)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={result === null ? "Result" : result + " kg.m/s"} />
+                    <Form.Text className="text-muted">
+                        Enter Mass & Velocity to Calculate the Momentum.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => setResult(null)} type="reset">
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+    // Force Calculator
+    function CalculatorForce() {
+        const [result, setResult] = useState(null)
+        const [mass, setMass] = useState(null)
+        const [acceleration, setAcce] = useState(null)
+
+        const handleClick = () => {
+            let res = mass * acceleration;
+            setResult(res)
+        }
+       
+        return <React.Fragment>
+            <Form>
+
+               
+                <Form.Group className="mb-3" controlId="mass">
+                    <Form.Label> Mass (in Kg)</Form.Label>
+                    <Form.Control onChange={(e) => setMass(e.target.value)} type="number" placeholder="Enter mass of an object in kilograms" />
+                </Form.Group>
+                    <Form.Label> Acceleration (in m/s²)</Form.Label>
+                <Form.Group className="mb-3" controlId="acceleration">
+                    <Form.Control onChange={(e) => setAcce(e.target.value)} type="number" placeholder="Enter acceleration in metre per second square [m/s²]" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="momentum">
+                    <Form.Label>Force (F)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={result === null ? "Result" : result + " N or Kg.m.s² "} />
+                    <Form.Text className="text-muted">
+                        Enter the above values to Calculate.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>&nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => setResult(null)} type="reset">
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+
+    // Friction Calculator
+    function CalculatorFriction() {
+        const [result, setResult] = useState(null)
+        const [force, setForce] = useState(null)
+        const [coeff, setCoeff] = useState(null)
+
+        function Cos(Deg) {
+            return Math.cos(Deg * Math.PI / 180);
+        }
+
+        const handleClick = () => {
+            let res = force * coeff;
+            setResult(res)
+        }
+       
+        return <React.Fragment>
+            <Form>
+
+               
+                <Form.Group className="mb-3" controlId="force">
+                    <Form.Label> Force applied (N)</Form.Label>
+                    <Form.Control onChange={(e) => setForce(e.target.value)} type="number" placeholder="Enter Force applied" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="coeff">
+                    <Form.Label> coefficient of Friction (μ)</Form.Label>
+                    <Form.Control onChange={(e) => setCoeff(e.target.value)} type="number" placeholder="Enter coefficient of Friction (μ)" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="momentum">
+                    <Form.Label>Frictional Force (F)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={result === null ? "Result" : result + " N"} />
+                    <Form.Text className="text-muted">
+                        Enter the above values to Calculate.
+                    </Form.Text>                    
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>&nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => setResult(null)} type="reset">
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+    // Gravitation Calculator
+    function CalculatorGravitation() {
+        const [result, setResult] = useState(null)
+        const [mass_A, setMassA] = useState(null)
+        const [mass_B, setMassB] = useState(null)
+        const [distance, setDistance] = useState(null)
+
+        const handleClick = () => {
+            let res = ( 6.67 * mass_A * mass_B ) / ( distance * distance );
+            setResult(res)
+        }
+        return <React.Fragment>
+            <Form>
+
+                <Form.Group className="mb-3" controlId="mass_A">
+                    <Form.Label>Mass A (m)</Form.Label>
+                    <Form.Control onChange={(e) => setMassA(e.target.value)} type="number" placeholder="Enter Mass of body A in SI unit" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="mass_B">
+                    <Form.Label>Mass B (M)</Form.Label>
+                    <Form.Control onChange={(e) => setMassB(e.target.value)} type="number" placeholder="Enter Mass of body B in SI unit" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="distance">
+                    <Form.Label>Distance (D)</Form.Label>
+                    <Form.Control onChange={(e) => setDistance(e.target.value)} type="number" placeholder="Enter Distance in SI unit" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="Gravitational_Force">
+                    <Form.Label>Gravitational Force (F)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={result === null ? "Result" : result + " x 10 ^ -11 Newton"} />
+                    <Form.Text className="text-muted">
+                        Enter masses & distance to Calculate the Gravitational Force .
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => setResult(null)} type="reset" >
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+    // Error Measurement Calculator
+    function CalculatorErrorMeasurement() {
+        const [absolute, setResultAbs] = useState(null)
+        const [relative, setResultRel] = useState(null)
+        const [percentage, setResultPer] = useState(null)
+        const [x_measured, setXM] = useState(null)
+        const [x_actual, setXA] = useState(null)
+
+        const handleClick = () => {
+            let res_abs = Math.abs(x_measured - x_actual) ;
+            let res_rel = res_abs / x_actual ;
+            let res_per = res_rel * 100 ;
+            setResultAbs(res_abs);
+            setResultRel(res_rel);
+            setResultPer(res_per);
+        }
+        return <React.Fragment>
+            <Form>
+
+                <Form.Group className="mb-3" controlId="x_measured">
+                    <Form.Label>X Measured (XM)</Form.Label>
+                    <Form.Control onChange={(e) => setXM(e.target.value)} type="number" placeholder="Enter Measured Value" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="x_actual">
+                    <Form.Label>X Actual (XA)</Form.Label>
+                    <Form.Control onChange={(e) => setXA(e.target.value)} type="number" placeholder="Enter Actual Value" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="absolute">
+                    <Form.Label>Absolute Error (EA)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={absolute === null ? "Result" : absolute} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="relative">
+                    <Form.Label>Relative Error (ER)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={relative === null ? "Result" : relative} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="percentage">
+                    <Form.Label>Percentage Error (ER)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={percentage === null ? "Result" : percentage} />
+                    <Form.Text className="text-muted">
+                        Enter measured and actual values to calculate result .
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => {setResultAbs(null); setResultPer(null); setResultRel(null); }} type="reset">
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+    //Torque Calculator
+    function CalculatorTorque() {
+        const [result, setResult] = useState(null)
+        const [force, setForce] = useState(null)
+        const [distance, setDist] = useState(null)
+        const [angle, setAngle] = useState(null)
+
+        const handleClick = () => {
+            let res = force * distance * Math.sin(angle*Math.PI/180);;
+            setResult(res)
+        }
+       
+        return <React.Fragment>
+            <Form>
+
+               
+                <Form.Group className="mb-3" controlId="force">
+                    <Form.Label> Force (in Newton)</Form.Label>
+                    <Form.Control onChange={(e) => setForce(e.target.value)} type="number" placeholder="Enter force applied" />
+                </Form.Group>
+                    <Form.Label> Distance (in meter)</Form.Label>
+                <Form.Group className="mb-3" controlId="distance">
+                    <Form.Control onChange={(e) => setDist(e.target.value)} type="number" placeholder="Enter distance covered" />
+                </Form.Group>
+                <Form.Label> Angle (in degree)</Form.Label>
+                <Form.Group className="mb-3" controlId="angle">
+                    <Form.Control onChange={(e) => setAngle(e.target.value)} type="number" placeholder="Enter angle (sin θ)" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="torque">
+                    <Form.Label>Force (F)</Form.Label>
+                    <Form.Control readOnly type="number" placeholder={result === null ? "Result" : result + " N.m "} />
+                    <Form.Text className="text-muted">
+                        Enter the above values to Calculate.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" onClick={handleClick}>
+                    Calculate
+                </Button>&nbsp;&nbsp;&nbsp;
+                <Button variant="dark" onClick={() => setResult(null)} type="reset">
+                    Reset
+                </Button>
+            </Form>
+        </React.Fragment>
+    }
+
+
+    // Adding Calculators together
+    
+    function calC(key) {
+        let currentCall;
+        switch (key) {
+            case "Momentum":
+                currentCall = CalculatorMomentum();
+                break;
+            case "Force":
+                currentCall = CalculatorForce();
+                break;
+            case "Friction":
+                currentCall = CalculatorFriction();
+                break;
+            case "Gravitation":
+                currentCall = CalculatorGravitation();
+                break;
+            case "Error Measurements":
+                currentCall = CalculatorErrorMeasurement();
+                break;
+            case "Torque":
+                currentCall = CalculatorTorque();
+                break;
+            default:
+                break;
+        }
+        return currentCall;
+    }
+
+    if(details.topic==="work_power_energy"){
+        return(
+            <div className="mech__main">
+            <div className="mech__header">
+                <h1>Work Power Energy</h1>
+            </div>
+            <div className="mech__topics-card">
+                {
+                    WPE_list.map(data =>
+                        <React.Fragment key={data.topic}>
+                            <a href={`/classicalmechanics/calc/work_power_energy/${data.topic}`} style={{textDecoration:"none"}}>
+                            <Card className="a" key={data.topic} style={{ width: '18rem' }} style={{color:'black',textAlign:'center',fontSize:"20px"}}>
+                                <Card.Body > 
+                                    <div> {data.topic}</div>
+                                </Card.Body>
+                            </Card>
+                            </a>
+                        </React.Fragment>
+                    )
+                }
+            </div>
+        </div >
+        )
+    }
+    else{
+    return (
+        
+        <div className="Calculator__main">
+            <div className="Calculator__header">
+                <h1>{details.topic}:</h1>
+            </div>
+            <div className="Calculator__image">
+                <img src={details.image} rounded="true" alt={details.topic} />
+            </div>
+            <div className="Calculator__details">
+                <p>{details.details}</p>
+            </div>
+            <div className="Calculator__formula-text">
+                <h3>Working Formula:</h3>
+            </div>
+            <div className="Calculator__formula">
+                <h3>{details.formula}</h3>
+            </div>
+            <div className="Calculator__calc">
+                <h3>{details.topic} Calculator</h3>
+                <hr />
+                {
+                    calC(details.topic)
+                }
+            </div>
+            <div className="Calculator__process">
+                <h3> Process</h3>
+                <p>{details.process}</p>
+            </div>
+            <div className="Calculator__siunit">
+                <h3> S.I. Unit : {details.siunit}</h3>
+                <p></p>
+            </div>
+            <div className="Calculator__dimension">
+                <h3> Dimension : {details.dimension}</h3>
+                <p></p>
+            </div>
+        </div>
+    )
+            }
+}
+
+export default Calculator
---
+++ Components/Classical Mechanics/topics_data.js
@@ -0,0 +1,120 @@
+import momentum from '../../Images/Classical_mechanics/momentum.jpg';
+import friction from '../../Images/Classical_mechanics/friction.jpg';
+import gravitation from '../../Images/Classical_mechanics/gravitation.jpg';
+import force from '../../Images/Classical_mechanics/force.jpg';
+import error_measurement from '../../Images/Classical_mechanics/error.jpg';
+import torque from '../../Images/Classical_mechanics/torque.jpg'
+import work_power_energy from '../../Images/Classical_mechanics/wpe.jpg'
+
+
+export {
+    momentum,
+    force,
+    friction,
+    gravitation,
+    torque,
+    error_measurement,
+    work_power_energy
+}
+
+const Topics = [
+    {
+        topic: "Momentum",
+        image: momentum,
+        details: `Momentum is a vector quantity, defined as the product of the mass of a particle and its velocity. From Isaac Newton’s second law of motion we can see that the rate of change of momentum is equal to the force acting on the particle.
+        
+
+        From Newton’s second law it follows that, if a constant force acts on a particle for a given time, the product of force and the time interval is equal to the change in the momentum. Conversely, the momentum of a particle is a measurement of the time, required for a constant force to bring it to rest.`,
+        formula: "p = mv",
+        process: "Here, the linear momentum is defined by p, m is the mass of the system and v is its velocity. We can calculate the momentum (p) by multiplying Mass of the particle (m) with the velocity of the particle (v).",
+        siunit: "kg · m/s",
+        dimension: "ML/T"
+    },
+    {
+        topic: "Force",
+        image: force,
+        details: ` 
+        Force is an external agent capable of changing the state of rest or motion of a particular body. It has a magnitude and a direction. The direction towards which the force is applied is known as the direction of the force and the application of force is the point where force is applied. 
+        The Force can be measured using a spring balance.                                             
+        Force has different effects and here are some of them, 
+        Force can make a body that is at rest to move.
+        It can stop a moving body or slow it down.
+        It can accelerate the speed of a moving body.
+        It can also change the direction of a moving body along with its shape and size.
+
+        `,
+        formula: "f = ma",
+        process: `The force (F) required to move an object of mass (m) with an acceleration (a) is given by the formula F = m x a. So, force = mass multiplied by acceleration.`,
+        siunit: "N or Kg.m.s²",
+        dimension: "ML/T²"
+    },
+    {
+        topic: "Friction",
+        image: friction,
+        details: `Friction is a force that resists the sliding or rolling of one solid object over another.
+        Surface of an object is microscopically irreguler, thats why, when any two objects in contact try to be in motion relatively with the another one, a force apears at their contact point resisting the motion, this force is opposite to the direction of motion.`,
+        formula: "F = μN",
+        process: `F, is the frictional force, N is the normal force. The the friction is calculated by F = μN.
+        
+        μ is the coefficient of friction, when the two objects are in relative motion, we call it coefficient of kinetic friction and when they are static, it is called the static friction coefficient.`,
+        siunit: "kg · m/s",
+        dimension: "ML/T²"
+    },
+    {
+        topic: "Gravitation",
+        image: gravitation,
+        details: "Gravity also termed as gravitation, is a force that occurs among all material objects in the universe. For any two objects or units having non-zero mass, the force of gravity has a tendency to attract them toward each other. The Universal Law of Gravitation says that: Every object in the universe has the property to attract every other object with a force which is directly proportional to the product of their masses and inversely proportional to the square of the distance between them ",
+
+        formula: "F = GmM(d^-2)",
+        process: "The Force of Gravitation F , between two bodies / system of mass m and M , where d is the radius or distance between the two bodies and G is the constant of proportionality known as the universal gravitation constant ( 6.67 × 10 ^ -11 Newton - meter ^ 2 · kg ^ -2 ) , is equals to product of ( G , m and M ) divided by the ( d ^ 2 ) . SI unit for gravitation force is Newton . SI unit for universal gravitational constant is Newton - meter ^ 2 · Kg ^ -2 ",
+        siunit: "G = Nm²/Kg²",
+        dimension: "G = M^-1 L^3 T^-2"
+    },
+    {
+        topic: "Error Measurements",
+        image: error_measurement,
+        details: "Any measurement that you make is just an approximation, 100% accuracy is not possible. If you measure the same object two different times, the two measurements may not be exactly the same.  The difference between the two measurements is called a variation in the measurements. This variation introduces an unwanted but unavoidable uncertainty in our measurement. This uncertainty is called the Errors in measurement. This ‘error’ should not be confused with a ‘mistake’. Error, unlike mistake, does not mean that you got the wrong answer. It just means you didn’t get as close to the true value as possible. The errors in measurement are a mathematical way to show the uncertainty in the measurement.  It is the difference between the result of the measurement and the true value of what you were measuring. Absolute , Relative and Percentage error are three types of error with which every experimental scientist should be familiar.",
+
+        formula: ["E (Abs) = | X (measured) - X (actual) | ", <br/>,
+                  "E(Rel) = E(Abs) / X(actual) & ",
+                  "E(Per) = E(Rel) * 100% "],
+        process: "Absolute error in measurement refers to the actual amount of error in the measurement. The absolute error of the measurement shows how large the error actually is. \
+                  Relative error represents the ratio of the absolute error of the measurement to the accepted measurement. This way we can determine the magnitude of the absolute error in terms of the actual size of the measurement. \
+                  Errors in measurement may also be expressed in terms of percentage. This is similar to relative error except that the error here is converted to a percent value. The percentage error is found by multiplying the relative error by 100%.",
+        siunit: "NA",
+        dimension: "NA"
+    },
+    {
+        topic: "Torque",
+        image: torque,
+        details: `Torque is the measure of the force that can cause an object to rotate about an axis. Force is what causes an object to accelerate in linear kinematics. Similarly, torque is what causes an angular acceleration. Hence, torque can be defined as the rotational equivalent of linear force. The point where the object rotates is called the axis of rotation. In physics, torque is simply the tendency of a force to turn or twist. Different terminologies such as moment or moment of force are interchangeably used to describe torque.`,
+        formula: "T = F.r. sinθ",
+        process: `A simple way to calculate the magnitude of the torque is to first determine the lever arm and then multiply it times the applied force.
+
+        Now, from the above observation, we conclude that torque produced depends on the magnitude of the force and the perpendicular distance between the point about which torque is calculated and the point of application of force.`,
+        siunit: "N.m",
+        dimension: "ML²/T²"
+    },
+    {
+        topic: "work_power_energy",
+        image: work_power_energy,
+        details:"...."
+    },
+    {
+        topic: "Collision",
+    },
+    {
+        topic: "Circular Motion",
+    },
+    {
+        topic: "Torque",
+    },
+    {
+        topic: "Work Power Energy",
+    },
+    {
+        topic: "Kinematics",
+    }
+]
+
+export default Topics
