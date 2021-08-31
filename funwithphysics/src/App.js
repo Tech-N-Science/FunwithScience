1bd0b435061d8c9165e7d0fd1d8b67713812b823
Amit Kumar Mishra <akm43300@gmail.com>
2021-08-31 23:17:15 +0530

    update

--- funwithphysics/src/App.js
+++ funwithphysics/src/App.js
@@ -6,6 +6,7 @@ import React from 'react'
 import ClassicalMechanics from './Components/Classical Mechanics/ClassicalMechanics'
 import Calculator from './Components/Classical Mechanics/Topics/Calculator'
 import WPECalculator from './Components/Classical Mechanics/Topics/wpe_Calculator'
+import main_topic from './Components/Main_topic/main_topic'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route exact path="/about" component={Home} />
         <Route exact path="/contact" component={Home} />
         <Route exact path="/classicalmechanics" component={ClassicalMechanics} />
+        <Route exact path="/physics" component={main_topic} />
         <Route exact path="/classicalmechanics/calc/:topic" component={Calculator} />
         <Route exact path="/classicalmechanics/calc/:topic/:topic" component={WPECalculator} />
         <Redirect to="/" />
@@ -23,48 +25,5 @@ const App = () => {
     </React.Fragment>
   )
 }
-
-// function App() {
-//    function Force(){
-//     var a = parseInt(document.getElementById("mass").value);
-//     var b = parseInt(document.getElementById("acce").value);
-//     var F = a*b;
-//     var re = document.getElementById("result");
-//     var temp = "Force = " + a+" x "+b+"<br/>";
-//     temp += "Force = "+F+"N";
-//     re.innerHTML = temp;
-
-//    } 
-
-//   return (
-//   <div className="App">
-
-//      <header className="App-header">
-
-//         <p>
-//           Fun With Physics
-//         </p>
-
-//     <div classNameName = "wrapper">
-//       <h3 styles={{fontStyle: 'Montserrat'}}>Calculating Force using mass and acceleration</h3>
-//       <br/>
-//       <p>F = m * a</p><br/>
-//       <form action="">
-//       <div className="form-group" id='inputform'>
-//           <input type="text"  className="form__field" name="a" id="mass" placeholder="Enter mass" />
-//           <input type="text" name="b" className="form__field" id="acce" placeholder="Enter acceleration " /> <br/> <br/>
-//           <button className="btn btn-outline-light" type="button" onClick={Force}>Find</button>
-
-//       </div>
-//     </form>
-//       <p id="result"></p><br/><br/>
-//     </div>
-
-
-//     </header> 
-//   </div>
-
-//   );
-// }
 
 export default App;
---
+++ funwithphysics/src/Components/Main_topic/main_topic.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import classicalMechanics from "../../Images/Classical_Mechanics.jpg"
+import thermodynamics from "../../Images/Thermodynamics.jpg"
+import electromagnetism from "../../Images/electromagnetism.jpg"
+import relativity from "../../Images/relativity.jpg"
+import atomic from "../../Images/atomic_physics.jpg"
+import nuclear from "../../Images/nuclear.jpg"
+import photonics from "../../Images/photonics.jpg"
+import statistical from "../../Images/statistical.jpg";
+import '../Main_topic/main_topic.css'
+
+
+function main_topic() {
+    return (
+        <div>
+            <h3 className="learn-header" id="learn">Physics calculator</h3>
+            <div id="fh5co-course-categories" className="mt-5 p-5">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3 col-sm-6 col-xs-12 text-center animate-box">
+							<a style={{ color: "black" }} href="/classicalmechanics" className="services">
+								<span className="icon">
+									{/* <i className="icon-shop"></i> */}
+									<img alt="classicalMechanics" src={classicalMechanics} />
+								</span>
+								<div className="desc">
+									<h3>Classical mechanics</h3>
+								</div>
+							</a>
+							<p>Classical mechanics is a physical theory describing the motion of macroscopic objects, from projectiles to parts of machinery, and astronomical objects.</p>
+						</div>
+                        <div className="col-md-3 col-sm-6 col-xs-12 text-center animate-box">
+							<a style={{ color: "black" }} href="/classicalmechanics" className="services">
+								<span className="icon">
+									{/* <i className="icon-heart4"></i> */}
+									<img alt="thermodynamics" src={thermodynamics} />
+								</span>
+								<div className="desc">
+									<h3>Thermodynamics</h3>
+									
+								</div>
+							</a>
+							<p>Thermodynamics is the branch of physics that deals with the relationships between heat and other forms of energy.</p>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-lab2"></i> */}
+									<img alt="relativity" src={relativity} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Relativity</a></h3>
+									<p>Relativity is the notion that the laws of physics are the same everywhere, be it on Earth or in a far off corner of the universe.</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-banknote"></i> */}
+									<img alt="electromagnetism" src={electromagnetism} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Electromagnetism</a></h3>
+									<p>Electromagnetism is a branch of physics involving the study of the electromagnetic force, a type of physical interaction between electrically charged particles. </p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-photo"></i> */}
+									<img alt="" src={atomic} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Atomic physics</a></h3>
+									<p>Atomic physics is the scientific study of the structure of the atom, its energy states, and its interactions with other particles and with electric and magnetic fields.</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-home-outline"></i> */}
+									<img alt="nuclear" src={nuclear} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Nuclear Physics</a></h3>
+									<p>Nuclear physics is the study of the protons and neutrons at the centre of an atom and the interactions that hold them together in a space.</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-bubble3"></i> */}
+									<img alt="photonics" src={photonics} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Photonics</a></h3>
+									<p>Photonics is the physical science of light waves. It deals with the science behind the generation, detection and manipulation of light.</p>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-3 col-sm-6 text-center animate-box">
+							<div className="services">
+								<span className="icon">
+									{/* <i className="icon-world"></i> */}
+									<img alt="statistical" src={statistical} />
+								</span>
+								<div className="desc">
+									<h3><a href="/">Statistical Mechanics</a></h3>
+									<p>Statistical mechanics is a mathematical framework that applies statistical methods and probability theory to large assemblies of microscopic entities.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+        </div>
+    )
+}
+
+export default main_topic
