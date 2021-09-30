49bbaf9653eb0c7b14cef7aff032c086ecd175ed
Lais Frigério <laiss@gazetadopovo.com.br>
2021-09-30 08:44:44 -0300

    Redesign footer

--- funwithphysics/src/Components/Footer/Footer.js
+++ funwithphysics/src/Components/Footer/Footer.js
@@ -1,90 +1,11 @@
 
-import React from 'react'
-import './Footer.css'
+import React from 'react';
 import logo from '../../Images/Logo/logo.png';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <React.Fragment>
-
-            {/* <footer className="text-center text-lg-start bg-light pt-2">
-        <section className="">
-            <div className="container text-center text-md-start mt-4">
-            
-            <div className="row mt-3">
-               
-                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-3">
-                
-                <h6 className="text-uppercase fw-bold mb-4">
-                    <i className=""></i>Fun With Physics
-                </h6>
-                <p>
-                    Here you can use rows and columns to organize your footer content. Lorem ipsum
-                    dolor sit amet, consectetur adipisicing elit.
-                </p>
-                </div>            
-
-               
-                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                
-                <h6 className="text-uppercase fw-bold mb-4">
-                    Products
-                </h6>
-                <p>
-                    <a href="#!" className="text-reset">Angular</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">React</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">Vue</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">Laravel</a>
-                </p>
-                </div>
-               
-
-               
-                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                
-                <h6 className="text-uppercase fw-bold mb-4">
-                    Useful links
-                </h6>
-                <p>
-                    <a href="#!" className="text-reset">Pricing</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">Settings</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">Orders</a>
-                </p>
-                <p>
-                    <a href="#!" className="text-reset">Help</a>
-                </p>
-                </div>
-               
-
-               
-                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                
-                <h6 className="text-uppercase fw-bold mb-4">
-                    Contact
-                </h6>
-                </div>
-               
-            </div>
-            
-            </div>
-        </section>
-
-        <div className="text-center p-4 bg-dark">
-            © 2021 | Copyright@
-            <a className="text-reset fw-bold" href="/"> Fun With Physics</a>
-        </div>
-        </footer> */}
-
             <footer>
                 <div className="main-content">
                     <div className="left box">
@@ -129,7 +50,7 @@ const Footer = () => {
                         <h2 className="additional">Additional Links</h2>
                         <div className="content"></div>
                         <div className="center-bottom">
-                            <div>
+                            <div className="about-us">
                                 <a href="/"><span className="fas fa-user"></span></a>
                                 <span className="text">About Us</span>
                             </div>
