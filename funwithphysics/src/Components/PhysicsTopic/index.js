8fa6cdc931642847bcb7fa63b4d486b7e20549d1
Lais Frigério <laiss@gazetadopovo.com.br>
2021-10-02 11:19:06 -0300

    Replace a tag with Link component

--- funwithphysics/src/Components/PhysicsTopic/index.js
+++ funwithphysics/src/Components/PhysicsTopic/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import classicalMechanics from "../../Images/Classical_Mechanics.jpg"
 import thermodynamics from "../../Images/Thermodynamics.jpg"
 import electromagnetism from "../../Images/electromagnetism.jpg"
@@ -22,7 +23,7 @@ function PhysicsTopic() {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-3 col-sm-6 col-xs-12 text-center animate-box">
-							<a style={{ color: "black" }} href="/classicalmechanics" className="services">
+							<Link style={{ color: "black" }} to="/classicalmechanics" className="services">
 								<span className="icon">
 									{/* <i className="icon-shop"></i> */}
 									<img alt="classicalMechanics" src={classicalMechanics} />
@@ -30,11 +31,11 @@ function PhysicsTopic() {
 								<div className="desc">
 									<h3>Classical mechanics</h3>
 								</div>
-							</a>
+							</Link>
 							<p>Classical mechanics is a physical theory describing the motion of macroscopic objects, from projectiles to parts of machinery, and astronomical objects.</p>
 						</div>
                         <div className="col-md-3 col-sm-6 col-xs-12 text-center animate-box">
-							<a style={{ color: "black" }} href="/classicalmechanics" className="services">
+							<Link style={{ color: "black" }} to="/classicalmechanics" className="services">
 								<span className="icon">
 									{/* <i className="icon-heart4"></i> */}
 									<img alt="thermodynamics" src={thermodynamics} />
@@ -43,7 +44,7 @@ function PhysicsTopic() {
 									<h3>Thermodynamics</h3>
 									
 								</div>
-							</a>
+							</Link>
 							<p>Thermodynamics is the branch of physics that deals with the relationships between heat and other forms of energy.</p>
 						</div>
 						<div className="col-md-3 col-sm-6 text-center animate-box">
@@ -65,7 +66,7 @@ function PhysicsTopic() {
 									<img alt="electromagnetism" src={electromagnetism} />
 								</span>
 								<div className="desc">
-									<h3><a href="/">Electromagnetism</a></h3>
+									<h3><Link to="/">Electromagnetism</Link></h3>
 									<p>Electromagnetism is a branch of physics involving the study of the electromagnetic force, a type of physical interaction between electrically charged particles. </p>
 								</div>
 							</div>
@@ -77,7 +78,7 @@ function PhysicsTopic() {
 									<img alt="" src={atomic} />
 								</span>
 								<div className="desc">
-									<h3><a href="/">Atomic physics</a></h3>
+									<h3><Link to="/">Atomic physics</Link></h3>
 									<p>Atomic physics is the scientific study of the structure of the atom, its energy states, and its interactions with other particles and with electric and magnetic fields.</p>
 								</div>
 							</div>
@@ -89,7 +90,7 @@ function PhysicsTopic() {
 									<img alt="nuclear" src={nuclear} />
 								</span>
 								<div className="desc">
-									<h3><a href="/">Nuclear Physics</a></h3>
+									<h3><Link to="/">Nuclear Physics</Link></h3>
 									<p>Nuclear physics is the study of the protons and neutrons at the centre of an atom and the interactions that hold them together in a space.</p>
 								</div>
 							</div>
@@ -101,7 +102,7 @@ function PhysicsTopic() {
 									<img alt="photonics" src={photonics} />
 								</span>
 								<div className="desc">
-									<h3><a href="/">Photonics</a></h3>
+									<h3><Link to="/">Photonics</Link></h3>
 									<p>Photonics is the physical science of light waves. It deals with the science behind the generation, detection and manipulation of light.</p>
 								</div>
 							</div>
@@ -113,7 +114,7 @@ function PhysicsTopic() {
 									<img alt="statistical" src={statistical} />
 								</span>
 								<div className="desc">
-									<h3><a href="/">Statistical Mechanics</a></h3>
+									<h3><Link to="/">Statistical Mechanics</Link></h3>
 									<p>Statistical mechanics is a mathematical framework that applies statistical methods and probability theory to large assemblies of microscopic entities.</p>
 								</div>
 							</div>
