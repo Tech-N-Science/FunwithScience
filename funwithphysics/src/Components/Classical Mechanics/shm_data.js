c2ae81a52715506407b87c05ffd16e986d0df518
Sourav Seal <sourav.seal1999@gmail.com>
2021-10-13 18:50:04 +0530

    added: shm topic images

--- funwithphysics/src/Components/Classical Mechanics/shm_data.js
+++ funwithphysics/src/Components/Classical Mechanics/shm_data.js
@@ -1,8 +1,16 @@
+import waves from '../../Images/Classical_mechanics/shm/waves.jpg';
+import oscillation from '../../Images/Classical_mechanics/shm/oscillation.png';
+
+export {
+    waves,
+    oscillation
+}
+
 const Topics = [
     {
         topic: "Waves",
-        // image: work,
-        details: `Waves can be described as a disturbance which propagates through a medium`,
+        image: waves,
+        details: `Waves can be described as a dynamic disturbance of one or more than one quantities, which propagates through a medium.`,
         formula: "Work = Force × Displacement = F × d",
         process: "When we kick a football, we are exerting an external force called F and due to this force (kick), the ball moves to a certain distance. This disposition of ball from position A to B is known as displacement (d). This work is said to be done and can be calculated as W = F × d ",
         siunit: "Joule or Newton-Meter",
@@ -10,7 +18,7 @@ const Topics = [
     },
     {
         topic: "Oscillation",
-        // image: power,
+        image: oscillation,
         details: `Power is a physical concept that has several different meanings, depending on the context and the information that is available. We can define power as the rate of doing work. It is the amount of energy consumed per unit time.`,
         formula: "P = W/t",
         process: "As discussed power is the rate of doing work. Therefore, it can be calculated by dividing work done by time. ",
