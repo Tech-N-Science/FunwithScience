2c7922c6a5db5afab75b228b470be003d3699a71
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-10-15 17:10:41 +0530

    KTG calculator added

---
+++ funwithphysics/src/Components/Thermodynamics/topics_data.js
@@ -0,0 +1,20 @@
+import ktg from "../../Images/Thermodynamics/KTG.png";
+
+export { ktg };
+var linebreak='\n'
+const Topics = [
+  { 
+    topic: "Kinetic Theory of Gases",
+    image: ktg,
+    details: `Kinetic Theory of Gases states that gaseous particles are in constant motion and undergo perfectly elastic collisions. In a collection of gas particles, the average kinetic energy is directly proportional to absolute temperature.
+
+    According to this theory, the gas molecule is composed of a huge number of tiny molecules compared to the distances between them. The kinetic theory of gases is necessary for clarifying the process of trapping particles by the diffusion mechanism.`,
+    formula: "P.V=n.R.T",
+    process:
+      "The Kinetic theory of gases is based on the Universal Gas Equation and all quantities related to it are derived from this. The required values can be easily found by punching in the other values in the equation and this calculator can very easily do that.",
+    siunit: "N/m² , m³ , mol , J/K.mol , K  ",
+    dimension: "M/L.T², M³, Mol, M.L²/T².K",
+  },
+];
+
+export default Topics;
