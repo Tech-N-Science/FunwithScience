cda798b718592ee839ed2b7cdb12d7603eae7851
Sourav Seal <sourav.seal1999@gmail.com>
2021-10-24 17:46:27 +0530

    added other topics

--- funwithphysics/src/Components/Thermodynamics/topics_data.js
+++ funwithphysics/src/Components/Thermodynamics/topics_data.js
@@ -3,7 +3,7 @@ import ktg from "../../Images/Thermodynamics/KTG.png";
 export { ktg };
 
 const Topics = [
-  { 
+  {
     topic: "Kinetic Theory of Gases",
     image: ktg,
     details: `Kinetic Theory of Gases states that gaseous particles are in constant motion and undergo perfectly elastic collisions. In a collection of gas particles, the average kinetic energy is directly proportional to absolute temperature.
@@ -14,6 +14,18 @@ const Topics = [
       "The Kinetic theory of gases is based on the Universal Gas Equation and all quantities related to it are derived from this. The required values can be easily found by punching in the other values in the equation and this calculator can very easily do that.",
     siunit: "N/m² , m³ , mol , J/K.mol , K  ",
     dimension: "M/L.T², M³, Mol, M.L²/T².K",
+  },
+  {
+    topic: "First law",
+  },
+  {
+    topic: "Second law",
+  },
+  {
+    topic: "Entropy",
+  },
+  {
+    topic: "Energy",
   },
 ];
 
