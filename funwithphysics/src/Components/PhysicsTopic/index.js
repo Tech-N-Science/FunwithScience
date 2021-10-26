1c64f8e7a490bef7c971c0a1a48fe7228d478f02
Amit Kumar Mishra <akm43300@gmail.com>
2021-10-26 12:01:44 +0530

    update

--- funwithphysics/src/Components/PhysicsTopic/index.js
+++ funwithphysics/src/Components/PhysicsTopic/index.js
@@ -41,26 +41,31 @@ function PhysicsTopic() {
             <div id="fh5co-course-categories" className="mt-5 p-5">
 				<div className="container">
 					<div className="row">
-					<PhysicsTopicItem name="Classical mechanics" link="/classicalmechanics" icon="icon-shop" image={classicalMechanics}>
+					<PhysicsTopicItem name="Classical mechanics" link="/classicalmechanics" icon="icon-shop" image={classicalMechanics} >
 							<p>Classical mechanics is a theory describing the motion of macroscopic objects, projectile motions of parts of machinery and astronomical objects and also rotatory motion of machines. </p>
+							<p id="credit">Photo by <a href="https://unsplash.com/@gabriele_macera?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Gab</a> on <a href="https://unsplash.com/s/photos/engine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
 							</PhysicsTopicItem>
 					<PhysicsTopicItem name="Thermodynamics" link="/thermodynamics" icon="icon-heart4" image={thermodynamics}>
 							<p>Thermodynamics is the branch of physics that deals with the transfer of heat and energy from one body to another.</p>
-							</PhysicsTopicItem>
+							 <p id="credit">  Photo by <a href="https://unsplash.com/@vivianeokubo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Viviane Okubo</a> on <a href="https://unsplash.com/s/photos/heat-transfer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+  							</PhysicsTopicItem>
 						<PhysicsTopicItem name="Relativity" link="/relativity" icon="icon-lab2" image={relativity}>
 							<p>Relativity is the notion that the laws of physics are the same everywhere, be it on Earth or in a far off corner of the universe.</p>
 						</PhysicsTopicItem>
 						<PhysicsTopicItem name="Electromagnetism" link="/electromagnetism" icon="icon-banknote" image={electromagnetism}>
 							<p>Electromagnetism is a branch of physics involving the study of the electromagnetic force, a type of physical interaction between electrically charged particles. </p>
+							<p id="credit">Photo by <a href="https://unsplash.com/@mbaumi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mika Baumeister</a> on <a href="https://unsplash.com/s/photos/magnet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
 						</PhysicsTopicItem>
 						<PhysicsTopicItem name="Atomic physics" link="/atomic" icon="icon-photo" image={atomic}>
 							<p>Atomic physics is the scientific study of the structure of the atom, its energy states, and its interactions with other particles and with electric and magnetic fields.</p>
+						    <p id="credit">Photo by <a href="https://unsplash.com/@vlisidis?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Terry Vlisidis</a> on <a href="https://unsplash.com/s/photos/atoms?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
 						</PhysicsTopicItem>
 						<PhysicsTopicItem name="Nuclear Physics" link="/nuclear" icon="icon-home-outline" image={nuclear}>
 							<p>Nuclear physics is the study of the protons and neutrons at the centre of an atom and the interactions that hold them together in a space.</p>
 						</PhysicsTopicItem>
 						<PhysicsTopicItem name="Photonics" link="/photonics" icon="icon-bubble3" image={photonics}>
 							<p>Photonics is the physical science of light waves. It deals with the science behind the generation, detection and manipulation of light.</p>
+						    <p id="credit">Photo by <a href="https://unsplash.com/@mischievous_penguins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Casey Horner</a> on <a href="https://unsplash.com/s/photos/light?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
 						</PhysicsTopicItem>
 						<PhysicsTopicItem name="Statistical Mechanics" link="/statistical" icon="icon-world" image={statistical}>
 							<p>Statistical mechanics is a mathematical framework that applies statistical methods and probability theory to large assemblies of microscopic entities.</p>
