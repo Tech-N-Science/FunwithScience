6361293aedd51dae4e3d062cfd034a02570dc83e
beingdevrahul <beingdevrahul@gmail.com>
2021-11-23 08:49:14 +0530

    update

--- funwithphysics/src/Components/Electromagnetism/topics_data.js
+++ funwithphysics/src/Components/Electromagnetism/topics_data.js
@@ -2,6 +2,15 @@ const Topics = [
     {
         topic:"Electric Field",
         details:"....",
+    },
+    {
+        topic:"Ohm's Law",
+        details:`Ohm's law states that the voltage (V) between two point is directly proportional to the current (I) across that two point and the constant of proportionality is resistance (R) and the equation is given as "V=IR"`,
+        siunit:["Resistance  : Ohm", <br/>,"Current : (A)", <br/>,"Voltage : (V)"],
+        process:"To find the voltage (V), we need to know the the current between the two point and the resistance(R) across the points & To find the current(I) or the resistance(R) we can also find it using the ohm's law",
+        formula:"V=IR",
+        dimension:"",
+
     }
 ]
 export default Topics;
