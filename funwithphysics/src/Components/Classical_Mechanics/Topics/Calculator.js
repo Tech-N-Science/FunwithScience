636d8a0ad867ec3782e1f16bd1c7d451a4033f19
Amit Kumar Mishra <akm4300@gmail.com.com>
2021-11-29 17:58:54 +0530

    update

--- funwithphysics/src/Components/Classical_Mechanics/Topics/Calculator.js
+++ funwithphysics/src/Components/Classical_Mechanics/Topics/Calculator.js
@@ -343,8 +343,8 @@ function Calculator({ match }) {
         formula: "U = -GMm/r",
         process: 
           "Consider a source mass ‘M’ is placed at a point along the x-axis, initially, a test mass ‘m’ is at infinity and G is the constant of proportionality known as the universal gravitation constant ( 6.67 × 10⁻¹¹ Newton - meter² · kg⁻² ) therefore gravitational potential energy (U) at a point which is at a distance ‘r’ from the source mass is equal to the product of negative of (G, M and m) divided by (r).",
-        siunit: " J/Kg",
-        dimension: "U = L² T⁻²",
+        siunit: " Joule",
+        dimension: "U = M¹ L² T⁻²",
       },
   ];
 
