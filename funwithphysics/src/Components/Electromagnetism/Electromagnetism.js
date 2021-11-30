603841c3db39b3e3c9b88a2500ba0506461226b2
Amit Kumar Mishra <akm4300@gmail.com.com>
2021-11-30 12:01:24 +0530

    update

--- funwithphysics/src/Components/Electromagnetism/Electromagnetism.js
+++ funwithphysics/src/Components/Electromagnetism/Electromagnetism.js
@@ -14,11 +14,20 @@ export default function Electromagnetism() {
         {
             topic:"Ohm's Law",
             details:`Ohm's law states that the voltage (V) between two points is directly proportional to the current (I) across that two points and the constant of proportionality is resistance (R) and the equation is given as "V=IR"`,
-            siunit:["Resistance  : Ohm", <br/>,"Current : (A)", <br/>,"Voltage : (V)"],
+            siunit:["Resistance  : Ohm", <br/>,"Current : (Ampere)", <br/>,"Voltage : (V)"],
             process:"To find the voltage (V), we need to know the the current between the two point and the resistance(R) across the points & To find the current(I) or the resistance(R) we can also find it using the ohm's law.",
             formula:"V=IR",
-            dimension:"",
+            dimension:"V = ML²T⁻²I⁻¹, R = ML²T⁻³I⁻²",
       
+        },
+        {
+            topic:"Drift Velocity",
+            details:`Subatomic particles, such as electrons, constantly move in random directions. When electrons are subjected to an electric field, they move randomly, but slowly in one direction, the direction of the applied electric field. Drift velocity is the net velocity at which these electrons drift.`,
+            siunit: 'm/s',
+            process:"To find the drift velocity (V), we need to know the the current flowing through the conductor(I), number of electrons(n), Area of cross-section of the conductor(A) and the charge of electron(q).",
+            formula:"V = I/nAq",
+            dimension:"M⁻¹T²I",
+        
         },
         {
             topic:"Electric Potential",
