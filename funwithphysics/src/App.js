5ae687237b67b7de70a39ec74d9244dfe466bbeb
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2021-12-02 20:39:48 +0530

    Update App.js

--- funwithphysics/src/App.js
+++ funwithphysics/src/App.js
@@ -1,7 +1,6 @@
+import Home from "./Components/Home/Home";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./index.css";
-import Navbar from "./Components/Navbar/Navbar"
-import Loadingimg from "./Images/Logo/logo.webp"
 import React, { Suspense, lazy } from "react";
 import ClassicalMechanics from "./Components/Classical_Mechanics/ClassicalMechanics";
 import Thermodynamics from "./Components/Thermodynamics/Thermodynamics";
@@ -17,20 +16,18 @@ import Quiz from "./Components/Quiz/Quiz";
 import FluidCalculator from "./Components/Classical_Mechanics/Topics/fluidmechanics_Calculator";
 import Electromagnetism from "./Components/Electromagnetism/Electromagnetism";
 import  electricfield_calculator  from "./Components/Electromagnetism/Topics/electricfield_calculator";
+import  Magneticfield_calculator  from "./Components/Electromagnetism/Topics/Magneticfield_calculator";
 import { electricpotential_calculator } from "./Components/Electromagnetism/Topics/electricpotential_calculator";
-import CalculatorElec from "./Components/Electromagnetism/Topics/calculator";
+import calculatorElec from "./Components/Electromagnetism/Topics/calculator";
 import {Helmet} from "react-helmet"
 import NumMCQ from "./Components/NumMCQ/NumMCQ";
 import Singlequestion from "./Components/NumMCQ/Singlequestion";
+// import { Helmet } from "react-helmet"
 import { hydrate, render } from "react-dom";
-import flux_calculator from "./Components/Electromagnetism/Topics/flux_calculator";
-const Home = lazy(() => {
-  return Promise.all([
-    import("./Components/Home/Home"),
-    new Promise(resolve => setTimeout(resolve, 4000))
-  ])
-  .then(([moduleExports]) => moduleExports);
-});
+// import Electromagnetism from "./Components/Electromagnetism/Electromagnetism";
+// import  electricfield_calculator  from "./Components/Electromagnetism/Topics/electricfield_calculator";
+// import calculatorElec from "./Components/Electromagnetism/Topics/calculator";
+const Navbar = lazy(()=>import("./Components/Navbar/Navbar"));
 
 const App = () => {
   return (
@@ -44,163 +41,133 @@ const App = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
       </Helmet>
+      <Suspense fallback={<div>Loading....</div>}>
+      <Navbar />
+      </Suspense>
       <Switch>
-      <Route exact path="/">
-        <Suspense fallback={<div className="loadingdiv"><img className="loadingimg" src={Loadingimg} alt=""/></div>}>
-        <Home/>
-        </Suspense>
-        </Route> 
-        <Route exact path="/about" component={Home}>
-        <Suspense fallback={<div className="loadingdiv"><img className="loadingimg" src={Loadingimg} alt=""/></div>}>
-          <Home/>
-        </Suspense>
-        </Route>
-        <Route exact path="/contact" component={Home}>
-        <Suspense fallback={<div className="loadingdiv"><img className="loadingimg" src={Loadingimg} alt=""/></div>}>
-          <Home/>
-         </Suspense>
-        </Route>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={Home} />
+        <Route exact path="/contact" component={Home} />
         <Route
           exact
           path="/classicalmechanics"
-        >
-          <Navbar/>
-          <ClassicalMechanics/>
-        </Route>
+          component={ClassicalMechanics}
+        />
         <Route
           exact
-          path="/thermodynamics">
-        <Navbar/>
-        <Thermodynamics/>
-        </Route>
-        <Route exact path="/physics">
-        <Navbar/>
-        <PhysicsTopic/> 
-        </Route>
+          path="/thermodynamics"
+          component={Thermodynamics}
+        />
+        
+        <Route exact path="/physics" component={PhysicsTopic} />
        
         <Route
           exact
           path="/classicalmechanics/calc/:topic"
           component={CalcClassic}
-        >
-            {/* <Navbar/> */}
-            {/* <CalcClassic/> */}
-        </Route>
+        />
         <Route
           exact
           path="/thermodynamics/calc/:topic"
           component={CalcThermo}
-        >
-        </Route>
+        />
         <Route
           exact
           path="/classicalmechanics/calc/gravitation/:topic"
           component={GravitationCalculator}
-        >
-        </Route>
+        />
         <Route
           exact
           path="/classicalmechanics/calc/moment_of_inertia/:topic"
           component={MOICalculator}
-        >
-        </Route>
+        />
         <Route
           exact
           path="/classicalmechanics/calc/shm/:topic"
           component={shm_Calculator} 
-        >
-        </Route>
+        />
         <Route
           exact
           path="/classicalmechanics/calc/work_power_energy/:topic"
           component={WPECalculator}
-        >
-        </Route>
+        />
         <Route
           exact
           path="/classicalmechanics/calc/rotation/:topic"
           component={rotation_Calculator}
-        >
-        </Route>
+        />
         <Route
           exact
           path="/electromagnetism"
-        >
-          <Navbar/>
-          <Electromagnetism/>
-        </Route>
+          component={Electromagnetism}
+        />
         <Route
           exact
           path="/electromagnetism/calc/:topic"
-          component={CalculatorElec}
-        >
-        </Route>
-
-        <Route
-          exact
-          path="/electromagnetism/calc/electric_field/:topic"
-          component={electricfield_calculator} 
-        >
-        </Route>
-
-        <Route exact path="/quiz">
-        <Navbar/>
-        <Quiz/>
-        </Route>
-
-        <Route
-          exact
-          path="/classicalmechanics/calc/fluid_mechanics/:topic"
-          component={FluidCalculator}
-        >
-        </Route>
-        <Route
-          exact
-          path="/electromagnetism"
-        >
-          <Navbar/>
-          <Electromagnetism/>
-        </Route>
-        <Route
-          exact
-          path="/electromagnetism/calc/:topic"
-          component={CalculatorElec}
-        >
-        </Route>
+          component={calculatorElec}
+        />
 
         <Route
           exact
           path="/electromagnetism/calc/electric_field/:topic"
           component={electricfield_calculator}
-        >
-        </Route>
+          
+        />
         <Route
           exact
-          path="/electromagnetism/calc/Flux/:topic"
-          component={flux_calculator}
+          path="/electromagnetism/calc/magnetic_field/:topic"
+          component={Magneticfield_calculator}
+          
+        />
+
+        <Route exact path="/quiz" component={Quiz} />
+
+        <Route
+          exact
+          path="/classicalmechanics/calc/fluid_mechanics/:topic"
+          component={FluidCalculator}
+        />
+        <Route
+          exact
+          path="/electromagnetism"
+          component={Electromagnetism}
+        />
+        <Route
+          exact
+          path="/electromagnetism/calc/:topic"
+          component={calculatorElec}
+        />
+
+        <Route
+          exact
+          path="/electromagnetism/calc/electric_field/:topic"
+          component={electricfield_calculator}
 
         />
         <Route
           exact
           path="/electromagnetism/calc/electric_potential/:topic"
           component={electricpotential_calculator}
-        >
-        </Route>
+
+        />
+        <Route
+          exact
+          path="/electromagnetism/calc/magnetic_field/:topic"
+          component={Magneticfield_calculator}
+          
+        />
 
         <Route 
           exact 
           path="/questions"
-        >
-          <Navbar/>
-          <NumMCQ/>
-        </Route>
+          component={NumMCQ}
+        />
 
         <Route
           exact
           path="/questions/:id"
           component={Singlequestion}
-        >
-        </Route>
+        />
         
         <Redirect to="/" />
       </Switch>
