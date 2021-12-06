a315dff19c485f4171f45505eb6a30375192d8e7
Amit Kumar Mishra <akm43300@gmail.com>
2021-12-06 21:23:26 +0530

    update

--- funwithphysics/src/Components/Signup/Signup.js
+++ funwithphysics/src/Components/Signup/Signup.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import signimg from "../../Images/girl.jpg";
 import "./Signup.css";
 import axios from "axios";
@@ -37,7 +37,7 @@ export default class Signup extends Component {
     };
     //console.log(ob);
     axios
-      .post("http://localhost/funwithscience/insert.php", ob)
+      .post("http://localhost/funwithscience_backend/insert.php", ob)
       .then((res) => console.log(res.data));
 
     this.setState({
@@ -60,41 +60,47 @@ export default class Signup extends Component {
           <form onSubmit={this.onSubmit}>
             <p className="signhead">Sign Up</p>
             <div className="signdiv">
-            <span><i class="fas fa-user"></i></span>
-            <input
-              type="text"
-              className="forminput fontAwesome"
-              name="username"
-              placeholder="Username"
-              required
-              value={this.state.username}
-              onChange={this.onChangeUsername}
-            />
+              <span>
+                <i class="fas fa-user"></i>
+              </span>
+              <input
+                type="text"
+                className="forminput fontAwesome"
+                name="username"
+                placeholder="Username"
+                required
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+              />
             </div>
             <div className="signdiv">
-            <span><i class="fas fa-envelope"></i></span>
-            <input
-              type="email"
-              className="forminput fontAwesome"
-              name="email"
-              autoComplete="off"
-              placeholder="Email"
-              required
-              value={this.state.email}
-              onChange={this.onChangeEmail}
-            />
+              <span>
+                <i class="fas fa-envelope"></i>
+              </span>
+              <input
+                type="email"
+                className="forminput fontAwesome"
+                name="email"
+                autoComplete="off"
+                placeholder="Email"
+                required
+                value={this.state.email}
+                onChange={this.onChangeEmail}
+              />
             </div>
             <div className="signdiv">
-            <span><i class="fas fa-key"></i></span>
-            <input
-              type="password"
-              className="forminput fontAwesome"
-              name="pass"
-              placeholder="Password"
-              required
-              value={this.state.pass}
-              onChange={this.onChangePassword}
-            />
+              <span>
+                <i class="fas fa-key"></i>
+              </span>
+              <input
+                type="password"
+                className="forminput fontAwesome"
+                name="pass"
+                placeholder="Password"
+                required
+                value={this.state.pass}
+                onChange={this.onChangePassword}
+              />
             </div>
             <button type="submit" className="btn btn-primary signupbtn">
               Register
