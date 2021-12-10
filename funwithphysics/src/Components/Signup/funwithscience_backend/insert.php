04d04c150868512e93d05e1c2c11c368db54b074
Amit Kumar Mishra <akm43300@gmail.com>
2021-12-11 00:11:27 +0530

    update

--- funwithphysics/src/Components/Signup/funwithscience_backend/insert.php
+++ funwithphysics/src/Components/Signup/funwithscience_backend/insert.php
@@ -6,6 +6,10 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "funwithscience_backend";
 
 $postdata = file_get_contents("php://input");
 if(isset($postdata)&&!empty($postdata)){
