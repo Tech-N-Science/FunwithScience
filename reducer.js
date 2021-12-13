14c268503c2a1d7f18a3375876eefe127e99a8ae
Pragya Mittal <69578414+pragyamittal0@users.noreply.github.com>
2021-12-13 22:16:54 +0530

    Add files via upload

---
+++ reducer.js
@@ -0,0 +1,19 @@
+export const initialstate = {
+  user: null,
+};
+
+export default (initialState, action) => {
+  if (action.type === "Login") {
+    return {
+      ...initialState,
+      user: action.payload,
+    };
+  } else if (action.type === "Logout") {
+    return {
+      ...initialState,
+      user: null,
+    };
+  } else {
+    return initialState;
+  }
+};
