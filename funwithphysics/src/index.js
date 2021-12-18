0598b3298455ee81a6f38ea1537af6e3bbedc4e6
Amit Kumar Mishra <60662775+Amit366@users.noreply.github.com>
2021-12-18 16:37:06 +0530

    Revert "created Solution Component and added it to force Calculator and Gravi…"

--- funwithphysics/src/index.js
+++ funwithphysics/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 
 ReactDOM.render(
