284aa7143901cfb12e6c00ae84690ee03b5fe30b
Amit Kumar Mishra <akm43300@gmail.com>
2021-12-25 10:38:30 +0530

    update

--- funwithphysics/src/Components/Solution/allConstants.js
+++ funwithphysics/src/Components/Solution/allConstants.js
@@ -1,8 +1,8 @@
 //All Constants
 
 export const constant = {
-  c: "299,792,458 m/s",
-  G: "6.67430 × 10−11 N m2/kg2",
-  h: "4.1356676969 × 10−15 eV s",
-  boltzmann: "1.38 x 10−23 J/K",
+  c: "3 x 10⁸ m/s",
+  G: "6.67430 × 10−¹¹ N m2/kg2",
+  h: "4.1356676969 × 10−¹⁵ eV s",
+  boltzmann: "1.38 x 10−²³ J/K",
 };
