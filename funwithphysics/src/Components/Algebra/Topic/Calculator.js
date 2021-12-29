89e9f35287df2216182f49c8c5615cd3fc7f6aa8
Amit Kumar Mishra <akm43300@gmail.com>
2021-12-29 18:39:57 +0530

    update

--- funwithphysics/src/Components/Algebra/Topic/Calculator.js
+++ funwithphysics/src/Components/Algebra/Topic/Calculator.js
@@ -1,65 +1,65 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
-import Navbar from '../../Navbar/Navbar';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import Navbar from "../../Navbar/Navbar";
 
 function Calculator({ match }) {
   // topics_data
   const Topics = [
     {
-      topic: 'Permutation',
+      topic: "Permutation",
       details: `A permutation of a set is a loosely defined organisation of its members into a sequence or linear order, or a rearranging of its elements if the set is already sorted.
       The word "permutation" also refers to the act or process of changing the linear order of an ordered set.`,
-      formula: 'nPr = n!/(n-r)!',
+      formula: "nPr = n!/(n-r)!",
       process: [
-        'To find the permutation, first we need to find the value of n factorial and n - r factorial, then we need to devide them.',
+        "To find the permutation, first we need to find the value of n factorial and n - r factorial, then we need to devide them.",
         <br />,
         <b>E.g. </b>,
         <span>
-          10<b style={{ fontSize: 25 + 'px' }}>P</b>5 = 10!/(10-5)! = 30240
+          10<b style={{ fontSize: 25 + "px" }}>P</b>5 = 10!/(10-5)! = 30240
         </span>,
       ],
     },
     {
-      topic: 'Combination',
+      topic: "Combination",
       details: `A combination is a mathematical technique for determining the number of possible arrangements in a group of objects where the order of the items is irrelevant. Selecting r objects out of the given n objects is given by using the factorials. It is denoted by nCr.`,
-      formula: 'C = n!/r!(n-r)!',
+      formula: "C = n!/r!(n-r)!",
       process: [
-        'To calculate combinations, we will use the formula nCr = n! / (r! * (n - r)!), where n represents the number of items, and r represents the number of items being chosen at a time.',
+        "To calculate combinations, we will use the formula nCr = n! / (r! * (n - r)!), where n represents the number of items, and r represents the number of items being chosen at a time.",
         <br />,
         <b>E.g. </b>,
         <span>
-          10<b style={{ fontSize: 25 + 'px' }}>C</b>5 = 10!/5!(10-5)! = 252
+          10<b style={{ fontSize: 25 + "px" }}>C</b>5 = 10!/5!(10-5)! = 252
         </span>,
       ],
     },
     {
-      topic: 'Progression',
+      topic: "Progression",
       details: [
         `An Arithmetic Progression is a sequence of numbers in which the difference between any two subsequent terms is always the same.`,
         <br />,
         <br />,
-        'If the ratio of two consecutive terms is always the same, the sequence of numbers is denoted as a Geometric Progression.',
+        "The sequence of numbers is called a Geometric Progression if the ratio of two subsequent terms is always the same.",
       ],
-      formula: ['AP = a + d * (n-1)', <br />, 'GP = a * r ^ (n-1)'],
+      formula: ["AP = a + d * (n-1)", <br />, "GP = a * r ^ (n-1)"],
       process: [
         <b>E.g. </b>,
-        '2,4,6,8.. is an AP because difference between any two consecutive terms in the series (common difference) is same (4 – 2 = 6 – 4 = 8 – 6 = 2).',
+        "2,4,6,8.. is an AP because difference between any two consecutive terms in the series (common difference) is same (4 – 2 = 6 – 4 = 8 – 6 = 2).",
         <br />,
-        'If ‘a’(2) is the first term and ‘d’(2) is the common difference,',
+        "If ‘a’(2) is the first term and ‘d’(2) is the common difference,",
         <br />,
-        'nth term of an AP = a + (n-1) d',
+        "nth term of an AP = a + (n-1) d",
         <br />,
-        '5th term of the AP = 2 + (5-1) * 2 = 10',
+        "5th term of the AP = 2 + (5-1) * 2 = 10",
         <br />,
         <b>E.g. </b>,
-        '2,4,8,16 is a GP because ratio of any two consecutive terms in the series (common difference) is same (4 / 2 = 8 / 4 = 16 / 8 = 2)',
+        "2,4,8,16 is a GP because ratio of any two consecutive terms in the series (common difference) is same (4 / 2 = 8 / 4 = 16 / 8 = 2)",
         <br />,
-        'If ‘a’(2) is the first term and ‘r’(2) is the common ratio,',
+        "If ‘a’(2) is the first term and ‘r’(2) is the common ratio,",
         <br />,
-        'nth term of a GP = a rn-1',
+        "nth term of a GP = a rn-1",
         <br />,
-        '5th term of the GP = 2 * 2 ^ (5-1) = 32',
+        "5th term of the GP = 2 * 2 ^ (5-1) = 32",
         <br />,
       ],
     },
@@ -92,48 +92,48 @@ function Calculator({ match }) {
     return (
       <>
         <Form>
-          <Form.Group className='mb-4' controlId='text'>
-            <Form.Text className='text'>
+          <Form.Group className="mb-4" controlId="text">
+            <Form.Text className="text">
               <strong>
-                {' '}
+                {" "}
                 To find the Permutation, Enter the following values
               </strong>
               <br />
             </Form.Text>
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>Value in a Set (n)</Form.Label>
             <Form.Control
               onChange={(e) => setn(e.target.value)}
-              type='number'
-              placeholder={'Enter the value of n'}
-              value={n === null ? '' : n}
+              type="number"
+              placeholder={"Enter the value of n"}
+              value={n === null ? "" : n}
             />
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>Value in Sub-Set (r)</Form.Label>
             <Form.Control
               onChange={(e) => setr(e.target.value)}
-              type='number'
-              placeholder={'Enter the value of r'}
-              value={r === null ? '' : r}
+              type="number"
+              placeholder={"Enter the value of r"}
+              value={r === null ? "" : r}
             />
           </Form.Group>
 
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Control
               readOnly
-              type='number'
-              placeholder={result === null ? 'Result' : result + ' '}
+              type="number"
+              placeholder={result === null ? "Result" : result + " "}
             />
           </Form.Group>
         </Form>
-        <div className='button-custom-grp'>
-          <Button variant='primary' onClick={calcPermutation}>
+        <div className="button-custom-grp">
+          <Button variant="primary" onClick={calcPermutation}>
             Calculate
           </Button>
           &nbsp;&nbsp;&nbsp;
-          <Button variant='dark' onClick={() => reset()} type='reset'>
+          <Button variant="dark" onClick={() => reset()} type="reset">
             Reset
           </Button>
         </div>
@@ -164,48 +164,48 @@ function Calculator({ match }) {
     return (
       <>
         <Form>
-          <Form.Group className='mb-4' controlId='text'>
-            <Form.Text className='text'>
+          <Form.Group className="mb-4" controlId="text">
+            <Form.Text className="text">
               <strong>
-                {' '}
+                {" "}
                 To find the Permutation, Enter the following values
               </strong>
               <br />
             </Form.Text>
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>Value in a Set (n)</Form.Label>
             <Form.Control
               onChange={(e) => setn(e.target.value)}
-              type='number'
-              placeholder={'Enter the value of n'}
-              value={n === null ? '' : n}
+              type="number"
+              placeholder={"Enter the value of n"}
+              value={n === null ? "" : n}
             />
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>Value in Sub-Set (r)</Form.Label>
             <Form.Control
               onChange={(e) => setr(e.target.value)}
-              type='number'
-              placeholder={'Enter the value of r'}
-              value={r === null ? '' : r}
+              type="number"
+              placeholder={"Enter the value of r"}
+              value={r === null ? "" : r}
             />
           </Form.Group>
 
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Control
               readOnly
-              type='number'
-              placeholder={result === null ? 'Result' : result + ' '}
+              type="number"
+              placeholder={result === null ? "Result" : result + " "}
             />
           </Form.Group>
         </Form>
-        <div className='button-custom-grp'>
-          <Button variant='primary' onClick={calcCombination}>
+        <div className="button-custom-grp">
+          <Button variant="primary" onClick={calcCombination}>
             Calculate
           </Button>
           &nbsp;&nbsp;&nbsp;
-          <Button variant='dark' onClick={() => reset()} type='reset'>
+          <Button variant="dark" onClick={() => reset()} type="reset">
             Reset
           </Button>
         </div>
@@ -216,7 +216,7 @@ function Calculator({ match }) {
   //Pogression Calculator - AP/GP
   const Progression = () => {
     const [result, setResult] = useState(null);
-    const [choice, setChoice] = useState('AP');
+    const [choice, setChoice] = useState("AP");
     const [n, setN] = useState(null);
     const [fr, setFR] = useState(null);
     const [nth, setNth] = useState(null);
@@ -228,10 +228,10 @@ function Calculator({ match }) {
     }
     const calcResult = () => {
       let res, s;
-      if (choice === 'AP') {
+      if (choice === "AP") {
         res = Number(n) + Number(fr * (nth - 1));
         s = (nth / 2) * (Number(2 * n) + Number(fr * (nth - 1)));
-      } else if (choice === 'GP') {
+      } else if (choice === "GP") {
         res = Number(n) * Number(fr ** (nth - 1));
         s = (n * (fr ** nth - 1)) / (fr - 1);
       }
@@ -246,16 +246,16 @@ function Calculator({ match }) {
       setSum(null);
     }
     const choiceData = () => {
-      if (choice === 'AP')
+      if (choice === "AP")
         return {
-          name: 'Arithmetic Progression',
-          quantities: ['First Number', 'Common diffrence'],
+          name: "Arithmetic Progression",
+          quantities: ["First Number", "Common diffrence"],
           disable: true,
         };
-      else if (choice === 'GP') {
+      else if (choice === "GP") {
         return {
-          name: 'Geometric Progression',
-          quantities: ['First Number', 'Common ratio'],
+          name: "Geometric Progression",
+          quantities: ["First Number", "Common ratio"],
         };
       }
     };
@@ -263,78 +263,78 @@ function Calculator({ match }) {
       <>
         <Form>
           {/* dropdown */}
-          <Form.Group className='mb-4' controlId='choice'>
+          <Form.Group className="mb-4" controlId="choice">
             <Form.Label>Select the type of calculation</Form.Label>
             <Form.Control
-              as='select'
-              className='select-custom-res'
+              as="select"
+              className="select-custom-res"
               onChange={(e) => handleChange(e)}
             >
-              <option value='AP'>Arethmetic Progression</option>
-              <option value='GP'>Geometric Progression</option>
+              <option value="AP">Arethmetic Progression</option>
+              <option value="GP">Geometric Progression</option>
             </Form.Control>
           </Form.Group>
-          <Form.Group className='mb-4' controlId='text'>
-            <Form.Text className='text'>
+          <Form.Group className="mb-4" controlId="text">
+            <Form.Text className="text">
               <strong>
                 To find the {choiceData().name}, Enter the following values
               </strong>
               <br />
             </Form.Text>
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>{choiceData().quantities[0]}</Form.Label>
             <Form.Control
               onChange={(e) => setN(e.target.value)}
-              type='number'
-              placeholder={'Enter the First Number'}
-              value={n === null ? '' : n}
+              type="number"
+              placeholder={"Enter the First Number"}
+              value={n === null ? "" : n}
             />
           </Form.Group>
 
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>{choiceData().quantities[1]}</Form.Label>
             <Form.Control
               onChange={(e) => setFR(e.target.value)}
-              type='number'
-              placeholder={'Enter the ' + choiceData().quantities[1]}
-              value={fr === null ? '' : fr}
+              type="number"
+              placeholder={"Enter the " + choiceData().quantities[1]}
+              value={fr === null ? "" : fr}
             />
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>nth Number to obtain</Form.Label>
             <Form.Control
               onChange={(e) => setNth(e.target.value)}
-              type='number'
-              placeholder={'Enter the nth Number to obtain'}
-              value={nth === null ? '' : nth}
+              type="number"
+              placeholder={"Enter the nth Number to obtain"}
+              value={nth === null ? "" : nth}
             />
           </Form.Group>
-          <Form.Group className='mb-4'>
-            <Form.Label>Number at {nth ? nth : 'nth'} position</Form.Label>
+          <Form.Group className="mb-4">
+            <Form.Label>Number at {nth ? nth : "nth"} position</Form.Label>
             <Form.Control
               readOnly
-              type='number'
-              placeholder={result === null ? 'Result' : result}
+              type="number"
+              placeholder={result === null ? "Result" : result}
             />
           </Form.Group>
-          <Form.Group className='mb-4'>
+          <Form.Group className="mb-4">
             <Form.Label>
-              Sum of numbers till {nth ? nth : 'nth'} position
+              Sum of numbers till {nth ? nth : "nth"} position
             </Form.Label>
             <Form.Control
               readOnly
-              type='number'
-              placeholder={sum === null ? 'Result' : sum}
+              type="number"
+              placeholder={sum === null ? "Result" : sum}
             />
           </Form.Group>
         </Form>
-        <div className='button-custom-grp'>
-          <Button variant='primary' onClick={calcResult}>
+        <div className="button-custom-grp">
+          <Button variant="primary" onClick={calcResult}>
             Calculate
           </Button>
           &nbsp;&nbsp;&nbsp;
-          <Button variant='dark' onClick={() => reset()} type='reset'>
+          <Button variant="dark" onClick={() => reset()} type="reset">
             Reset
           </Button>
         </div>
@@ -346,13 +346,13 @@ function Calculator({ match }) {
   function calC(key) {
     let currentCall;
     switch (key) {
-      case 'Permutation':
+      case "Permutation":
         currentCall = Permutation();
         break;
-      case 'Combination':
+      case "Combination":
         currentCall = Combination();
         break;
-      case 'Progression':
+      case "Progression":
         currentCall = Progression();
         break;
       default:
@@ -363,31 +363,31 @@ function Calculator({ match }) {
   return (
     <React.Fragment>
       <Navbar />
-      <div className='Calculator__main'>
+      <div className="Calculator__main">
         <Helmet>
           <title>{details.topic}</title>
-          <meta name='description' content='{details.details}' />
+          <meta name="description" content="{details.details}" />
           <meta
-            name='keywords'
-            content='Algebra, calculator, Algebra calculator, physics, Tech n science, technscience, tech and science'
+            name="keywords"
+            content="Algebra, calculator, Algebra calculator, physics, Tech n science, technscience, tech and science"
           />
         </Helmet>
 
-        <div className='Calculator__header'>
+        <div className="Calculator__header">
           <h1>{details.topic}</h1>
         </div>
-        <div className='Calculator__details'>
+        <div className="Calculator__details">
           <p>{details.details}</p>
         </div>
-        <div className='Calculator__formula'>
+        <div className="Calculator__formula">
           <h3>Working Formula:</h3>
           <h3>{details.formula}</h3>
         </div>
-        <div className='Calculator__process'>
+        <div className="Calculator__process">
           <h3> Process</h3>
           <p>{details.process}</p>
         </div>
-        <div className='Calculator__calc'>
+        <div className="Calculator__calc">
           <h3>{details.topic} Calculator</h3>
           <hr />
           {calC(details.topic)}
