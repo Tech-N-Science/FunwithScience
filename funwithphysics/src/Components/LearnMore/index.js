ea65f54c55b075f5b66f2d29bfbd00039e979368
Arpita Naval <navalarpita@gmail.com>
2022-01-02 23:38:53 +0530

    Add animate on scroll

--- funwithphysics/src/Components/LearnMore/index.js
+++ funwithphysics/src/Components/LearnMore/index.js
@@ -3,26 +3,31 @@ import blackboardSvg from "../../Images/blackboard-svg.svg";
 import computerSvg from "../../Images/computer-svg.svg";
 import addCircleSvg from "../../Images/add-circle.svg";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import "./styles.css";
-
+import Aos from "aos";
+import "aos/dist/aos.css"
 export function LearnMore() {
+  useEffect(() => {
+    Aos.init({duration: 2000});
+  }, [])
   return (
     <section className="learn-more">
       <article className="list-computer">
         <ul>
-          <li>
+          <li data-aos="fade-in">
             <p>Networking</p>
             <small className="topic computer-topic">Computer</small>
             <img src={computerSvg} alt="Computer icon" />
             <Link to="/">+</Link>
           </li>
-          <li>
+          <li data-aos="fade-in">
             <p>DBMS</p>
             <small className="topic computer-topic">Computer</small>
             <img src={computerSvg} alt="Computer icon" />
             <Link to="/">+</Link>
           </li>
-          <li>
+          <li data-aos="fade-in">
             <p>Computational Physics</p>
             <small className="topic computer-topic">Computer</small>
             <img src={computerSvg} alt="Computer icon" />
@@ -33,7 +38,7 @@ export function LearnMore() {
 
       <article className="list-science">
         <ul>
-          <li className="science">
+          <li data-aos="fade-in" className="science">
             <p>Maths</p>
             <small className="topic science-topic">Science</small>
             <img src={blackboardSvg} alt="Icon" />
@@ -51,7 +56,7 @@ export function LearnMore() {
               </div>
             </div>
           </li>
-          <li className="science">
+          <li data-aos="fade-in" className="science">
             <p>Physics</p>
             <small className="topic science-topic">Science</small>
             <img src={blackboardSvg} alt="Icon" />
