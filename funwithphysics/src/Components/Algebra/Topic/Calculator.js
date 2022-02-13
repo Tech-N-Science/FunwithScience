c579d0f4c29e2bf1263a2edeb0c23eb85194f77d
Vatsal Mehta <vatsalmehta103@gmail.com>
2022-02-14 00:12:08 +0530

    Exponent Calculator Added

--- funwithphysics/src/Components/Algebra/Topic/Calculator.js
+++ funwithphysics/src/Components/Algebra/Topic/Calculator.js
@@ -4,7 +4,6 @@ import { Helmet } from "react-helmet";
 import Navbar from "../../Navbar/Navbar";
 import { useParams } from "react-router";
 import "./Calculator.css";
-import ResetPassword from "../../Forgot_Password/resetpassword";
 import { useEffect } from "react";
   
 
