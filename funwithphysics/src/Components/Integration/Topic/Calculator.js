2d1e64b6674351c3c79246ca659761675003f3db
Sabari Ganesh <64348740+SabariGanesh-K@users.noreply.github.com>
2022-02-16 22:31:08 +0530

    imported modules

--- funwithphysics/src/Components/Integration/Topic/Calculator.js
+++ funwithphysics/src/Components/Integration/Topic/Calculator.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import Navbar from "../../Navbar/Navbar";
 import { useParams } from "react-router";
 import "./Calculator.css";
+import { Form, Button } from "react-bootstrap";
 import limits1 from "../../../Images/limits.png
 
 function Calculator() {
