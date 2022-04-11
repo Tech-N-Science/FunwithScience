f3358f8db34f658b38f8a2b11dce9559a0cbb1df
RishikaKant <76899370+RishikaKant@users.noreply.github.com>
2022-04-11 13:18:10 +0530

    resolved conflicts

--- funwithphysics/src/Components/MathMCQ/data.js
+++ funwithphysics/src/Components/MathMCQ/data.js
@@ -1,30 +1,117 @@
-
 export const data = [
   {
     id: 1,
     type: "mcq",
-    question:
-      "Addition of 1 + 2 = ",
-    topic: "Topic1",
+    question:"The value of cos 0°. cos 1°. cos 2°. cos 3°… cos 89° cos 90° is",
+    topic: "Trignometry",
     answer: [
       {
         answerText:
-          "A)  5",
+          "A)  1",
         isCorrect: false,
       },
       {
         answerText:
-          "B)  6",
+          "B)  -1",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "C)  0",
         isCorrect: true,
       },
       {
         answerText:
-          "C)  7",
+          "D)  1/2",
+        isCorrect: false,
+      },
+    ]
+  },
+  {
+    id: 2,
+    type: "mcq",
+    question: "Roots of the equation x^n -1 = 0 where n belongs to Integers",
+    topic: "Complex Numbers",
+    answer: [
+      {
+        answerText: "A) form a regular polygon of unit circum-radius ",
+        isCorrect: false,
+      },
+      {
+        answerText: "B) lie on a circle",
+        isCorrect: false,
+      },
+      {
+        answerText: "C) are non-collinear",
+        isCorrect: false,
+      },
+      {
+        answerText: "D) A & B",
+        isCorrect: true,
+      },
+    ],
+    difficulty: "Easy",
+    image: "",
+  },
+  {
+    id: 3,
+    type: "mcq",
+    question:
+      "If |z| < 4, then | iz +3 – 4i| is less than ",
+    topic: "Complex Numbers",
+    answer: [
+      {
+        answerText:
+          "A)  4",
+        isCorrect: false,
+      },
+      {
+        answerText: "B)  5",
+        answerText:
+          "B)  5",
+        isCorrect: false,
+      },
+      {
+        answerText: "C)  6",
+        answerText:
+          "C)  6",
+        isCorrect: false,
+      },
+      {
+        answerText: "D)  7",
+        answerText:
+          "D)  9",
+        isCorrect: true,
+      },
+    ],
+    difficulty: "Medium",
+    image: "",
+  },
+  {
+    id: 4,
+    type: "mcq",
+    question:
+      "If y sin 45° cos 45° = tan2 45° – cos2 30°, then y = …",
+    topic: "Trignometry",
+    answer: [
+      {
+        answerText:
+          "A)  -1/2",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "B)  1/2",
         isCorrect: true,
       },
       {
         answerText:
-          "D)  8",
+          "C)  2",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "D)  -2",
         isCorrect: false,
       },
     ],
@@ -32,31 +119,191 @@ export const data = [
     image: "",
   },
   {
-    id: 2,
+    id: 5,
     type: "mcq",
-    question: "Subtraction of 5 from 2 ",
-    topic: "Topic2",
+    question:
+      "For a complex number z, the minimum value of |z| + | z - cos - isin| is",
+    topic: "Complex Numbers",
     answer: [
       {
-        answerText: "A)  3",
-        isCorrect: true,
-      },
-      {
-        answerText: "B)  5",
+        answerText:
+          "A)  0",
         isCorrect: false,
       },
       {
-        answerText: "C)  6",
+        answerText:
+          "B)  1",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "C)  2",
         isCorrect: false,
       },
       {
-        answerText: "D)  7",
+        answerText:
+          "D)  None of these",
+        isCorrect: false,
+      },
+    ],
+    difficulty: "Hard",
+    image: "",
+  },
+  {
+    id: 6,
+    type: "mcq",
+    question:
+      "If sin θ + sin² θ = 1, then cos² θ + cos4 θ = ..",
+    topic: "Trignometry",
+    answer: [
+      {
+        answerText:
+          "A)  -1",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "B)  0",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "C)  1",
         isCorrect: true,
+      },
+      {
+        answerText:
+          "D)  2",
+        isCorrect: false,
       },
     ],
     difficulty: "Easy",
     image: "",
   },
-  // Similarily add more questions
-  
-];
+  {
+    id: 7,
+    type: "mcq",
+    question:
+      "If z be any complex number such that |3z –2| + |3z +2| = 4, then locus of z is ",
+    topic: "Complex Numbers",
+    answer: [
+      {
+        answerText:
+          "A)  an ellipse ",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "B)  a circle",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "C)  a line-segment",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "D)  None of these ",
+        isCorrect: false,
+      },
+    ],
+    difficulty: "Medium",
+    image: "",
+  },
+  {
+    id: 8,
+    type: "mcq",
+    question:
+      "If the roots of 4x^2+ 5k = (5k + 1) x differ by unity then the negative value of k is",
+    topic: "Quadratic Equations",
+    answer: [
+      {
+        answerText:
+          "A)  -3",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "B)  -1/5",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "C)  -3/5",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "D)  None of these",
+        isCorrect: false,
+      },
+    ],
+    difficulty: "Easy",
+    image: "",
+  },
+  {
+    id: 9,
+    type: "mcq",
+    question:
+      "If the complex numbers z1, z2, z3 are in A.P., then they lie on a",
+    topic: "Complex Numbers",
+    answer: [
+      {
+        answerText:
+          "A)  circle",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "B)  parabola",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "C)  line",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "D)  ellipse",
+        isCorrect: false,
+      },
+    ],
+    difficulty: "Medium",
+    image: "",
+  },
+  {
+    id: 10,
+    type: "mcq",
+    question:
+      "The number of real solutions of the equations e^x= x is",
+    topic: "Quadratic Equations",
+    answer: [
+      {
+        answerText:
+          "A)  0",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "B)  1",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "C)  2",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "D)  Infinite",
+        isCorrect: false,
+      },
+    ],
+    difficulty: "Medium",
+    image: "",
+  },
+
+]
+//Similarly add more questions
