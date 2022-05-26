1224774abc18bbfacf8e743aa048de209f3d6578
Hazem Abdo <ha4450770@gmail.com>
2022-05-26 20:27:54 +0200

    handling no editorial for a question

--- funwithphysics/src/Components/Editorial/editorialData.js
+++ funwithphysics/src/Components/Editorial/editorialData.js
@@ -1,173 +1,132 @@
 export const editorialData = [
   {
+    type: "math",
+    id: 0,
+    // content is markdown format string
+    content: `
+    # Hello, *world*!
+    # A demo of \`react-markdown\`
+
+    \`react-markdown\` is a markdown component for React.
+    
+    👉 Changes are re-rendered as you type.
+    
+    👈 Try writing some markdown on the left.
+    
+    ## Overview
+    
+    * Follows [CommonMark](https://commonmark.org)
+    * Optionally follows [GitHub Flavored Markdown](https://github.github.com/gfm/)
+    * Renders actual React elements instead of using \`dangerouslySetInnerHTML\`
+    * Lets you define your own components (to render \`MyHeading\` instead of \`h1\`)
+    * Has a lot of plugins
+    
+    ## Table of contents
+    
+    Here is an example of a plugin in action
+    ([\`remark-toc\`](https://github.com/remarkjs/remark-toc)).
+    This section is replaced by an actual table of contents.
+    
+    ## Syntax highlighting
+    
+    Here is an example of a plugin to highlight code:
+    [\`rehype-highlight\`](https://github.com/rehypejs/rehype-highlight).
+    
+    \`\`\`js
+    import React from 'react'
+    import ReactDOM from 'react-dom'
+    import ReactMarkdown from 'react-markdown'
+    import rehypeHighlight from 'rehype-highlight'
+    
+    ReactDOM.render(
+      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{'# Your markdown here'}</ReactMarkdown>,
+      document.querySelector('#content')
+    )
+    \`\`\`
+    
+    Pretty neat, eh?
+    
+    ## GitHub flavored markdown (GFM)
+    
+    For GFM, you can *also* use a plugin:
+    [\`remark-gfm\`](https://github.com/remarkjs/react-markdown#use).
+    It adds support for GitHub-specific extensions to the language:
+    tables, strikethrough, tasklists, and literal URLs.
+    
+    These features **do not work by default**.
+    👆 Use the toggle above to add the plugin.
+    
+    | Feature    | Support              |
+    | ---------: | :------------------- |
+    | CommonMark | 100%                 |
+    | GFM        | 100% w/ \`remark-gfm\` |
+    
+    ~~strikethrough~~
+    
+    * [ ] task list
+    * [x] checked item
+    
+    https://example.com
+    
+    ## HTML in markdown
+    
+    ⚠️ HTML in markdown is quite unsafe, but if you want to support it, you can
+    use [\`rehype-raw\`](https://github.com/rehypejs/rehype-raw).
+    You should probably combine it with
+    [\`rehype-sanitize\`](https://github.com/rehypejs/rehype-sanitize).
+    
+    <blockquote>
+      👆 Use the toggle above to add the plugin.
+    </blockquote>
+    
+    ## Components
+    
+    You can pass components to change things:
+    
+    \`\`\`js
+    import React from 'react'
+    import ReactDOM from 'react-dom'
+    import ReactMarkdown from 'react-markdown'
+    import MyFancyRule from './components/my-fancy-rule.js'
+    
+    ReactDOM.render(
+      <ReactMarkdown
+        components={{
+          // Use h2s instead of h1s
+          h1: 'h2',
+          // Use a component instead of hrs
+          hr: ({node, ...props}) => <MyFancyRule {...props} />
+        }}
+      >
+        # Your markdown here
+      </ReactMarkdown>,
+      document.querySelector('#content')
+    )
+    \`\`\`
+    
+    ## More info?
+    
+    Much more info is available in the
+    [readme on GitHub](https://github.com/remarkjs/react-markdown)!
+    
+    ***
+    
+    A component by [Espen Hovlandsdal](https://espen.codes/)
+       `,
+  },
+  {
+    type: "physics",
     id: 1,
-    question: "The value of cos 0°. cos 1°. cos 2°. cos 3°… cos 89° cos 90° is",
-    topic: "Math",
- // content is markdown format string
-   content: 
-   `
-   An h1 header
-============
-
-Paragraphs are separated by a blank line.
-
-2nd paragraph. *Italic*, **bold**, and . Itemized lists
-look like:
-
-  * this one
-  * that one
-  * the other one
-
-Note that --- not considering the asterisk --- the actual text
-content starts at 4-columns in.
-
-> Block quotes are
-> written like so.
->
-> They can span multiple paragraphs,
-> if you like.
-
-Use 3 dashes for an em-dash. Use 2 dashes for ranges (ex., "it's all
-in chapters 12--14"). Three dots ... will be converted to an ellipsis.
-Unicode is supported. ☺
-
-
-
-An h2 header
-------------
-
-Here's a numbered list:
-
- 1. first item
- 2. second item
- 3. third item
-
-Note again how the actual text starts at 4 columns in (4 characters
-from the left side). Here's a code sample:
-
-    # Let me re-iterate ...
-    for i in 1 .. 10 { do-something(i) }
-
-As you probably guessed, indented 4 spaces. By the way, instead of
-indenting the block, you can use delimited blocks, if you like:
-
-~~~
-define foobar() {
-    print "Welcome to flavor country!";
-}
-~~~
-
-(which makes copying & pasting easier). You can optionally mark the
-delimited block for Pandoc to syntax highlight it:
-
-~~~python
-import time
-# Quick, count to ten!
-for i in range(10):
-    # (but not *too* quick)
-    time.sleep(0.5)
-    print(i)
-~~~
-
-
-
-### An h3 header ###
-
-Now a nested list:
-
- 1. First, get these ingredients:
-
-      * carrots
-      * celery
-      * lentils
-
- 2. Boil some water.
-
- 3. Dump everything in the pot and follow
-    this algorithm:
-
-        find wooden spoon
-        uncover pot
-        stir
-        cover pot
-        balance wooden spoon precariously on pot handle
-        wait 10 minutes
-        goto first step (or shut off burner when done)
-
-    Do not bump wooden spoon or it will fall.
-
-Notice again how text always lines up on 4-space indents (including
-that last line which continues item 3 above).
-
-Here's a link to [a website](http://foo.bar), to a [local
-doc](local-doc.html), and to a [section heading in the current
-doc](#an-h2-header). Here's a footnote [^1].
-
-[^1]: Some footnote text.
-
-Tables can look like this:
-
-Name           Size  Material      Color
-------------- -----  ------------  ------------
-All Business      9  leather       brown
-Roundabout       10  hemp canvas   natural
-Cinderella       11  glass         transparent
-
-Table: Shoes sizes, materials, and colors.
-
-(The above is the caption for the table.) Pandoc also supports
-multi-line tables:
-
---------  -----------------------
-Keyword   Text
---------  -----------------------
-red       Sunsets, apples, and
-          other red or reddish
-          things.
-
-green     Leaves, grass, frogs
-          and other things it's
-          not easy being.
---------  -----------------------
-
-A horizontal rule follows.
-
-***
-
-Here's a definition list:
-
-apples
-  : Good for making applesauce.
-
-oranges
-  : Citrus!
-
-tomatoes
-  : There's no "e" in tomatoe.
-
-Again, text is indented 4 spaces. (Put a blank line between each
-term and  its definition to spread things out more.)
-
-Here's a "line block" (note how whitespace is honored):
-
-| Line one
-|   Line too
-| Line tree
-
-and images can be specified like so:
-
-![example image](example-image.jpg "An exemplary image")
-
-Inline math equation: $\omega = d\phi / dt$. Display
-math should get its own line like so:
-
-$$I = \int \rho R^{2} dV$$
-
-And note that you can backslash-escape any punctuation characters
-which you wish to be displayed literally, ex.: \`foo\`, \*bar\*, etc.
-   `,
-
+    content: `
+    # Hello, *world*!
+    # A demo of \`react-markdown\`
+    ## Overview
+    * Follows [CommonMark](https://commonmark.org)
+    * Optionally follows [GitHub Flavored Markdown](https://github.github.com/gfm/)
+    * Renders actual React elements instead of using \`dangerouslySetInnerHTML\`
+    * Lets you define your own components (to render \`MyHeading\` instead of \`h1\`)
+    * Has a lot of plugins
+    `,
+  },
 ];
-//Similarly add more questions
 
-//branch-MathQuestionsUpdated
